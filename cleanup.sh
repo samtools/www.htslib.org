fffe97799fe9f04238d485bcf6c255ad065a949d
Martin O. Pollard <mp15@sanger.ac.uk>
2014-07-18 12:56:59 +0100

    Some initial work on new site for 1.0
    Clean up everything that was there, create site skeleton using bootstrap and plain HTML.

--- cleanup.sh
+++
@@ -1,5 +0,0 @@
-find . -name "*.shtml" | xargs rm
-find . -name "*~" | xargs rm
-if [ -f index.true.html ]; then
-	mv -f index.true.html index.html
-fi
--- upload.sh
+++
@@ -1,5 +0,0 @@
-./cleanup.sh
-find . -name "*.html" | xargs ./csi2ssi.pl
-mv index.html index.true.html
-rsync -Cavz * lh3lh3,samtools@web.sourceforge.net:htdocs/
-./cleanup.sh
